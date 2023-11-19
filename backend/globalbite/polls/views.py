8c52714287eae3d38900c8ed1d459c288e7ab56d
Ângela Coelho <up201907549@fe.up.pt>
2023-11-19 11:37:51 +0000

    Setup backend

---
+++ backend/globalbite/polls/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.´
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
