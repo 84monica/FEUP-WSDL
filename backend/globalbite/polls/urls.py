c01d4079c15ab851f2bd007f3313b8736cd5a5de
84monica <up201905753@fe.up.pt>
2023-11-25 18:44:53 +0000

    Listing countries and recipes from one specific country

--- backend/globalbite/polls/urls.py
+++ backend/globalbite/polls/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("recipes/", views.recipe_list, name="recipes"),
+    path("countries/", views.country_list, name="countries"),
+    path('country/<str:country>/', views.country_recipes, name='country_recipes'),
 ]
--- backend/globalbite/polls/views.py
+++ backend/globalbite/polls/views.py
@@ -5,8 +5,25 @@ from .models import Recipe
 # Create your views here.´
 
 def index(request):
-    template = loader.get_template("polls/index.html")
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def recipe_list(request):
+    template = loader.get_template("polls/recipes.html")
     context = {
         "recipes": Recipe.objects.all(),
+    }
+    return HttpResponse(template.render(context, request))
+
+def country_list(request):
+    template = loader.get_template("polls/countries.html")
+    context = {
+        "countries": Recipe.objects.values_list('country_of_origin', flat=True).distinct().order_by('country_of_origin'),
+    }
+    return HttpResponse(template.render(context, request))
+
+def country_recipes(request, country):
+    template = loader.get_template("polls/recipes.html")
+    context = {
+        "recipes": Recipe.objects.filter(country_of_origin=country),
     }
     return HttpResponse(template.render(context, request))
