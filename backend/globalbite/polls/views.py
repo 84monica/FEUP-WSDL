48fcf55d24205dad5db6b6e329dff83cbfc078b6
84monica <up201905753@fe.up.pt>
2023-11-30 18:14:32 +0000

    feat: Improve layout of the MainPage

--- backend/globalbite/polls/views.py
+++ backend/globalbite/polls/views.py
@@ -1,29 +1,35 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.db.models import Count
 from .models import Recipe
 
 # Create your views here.´
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def recipe_list(request):
-    template = loader.get_template("polls/recipes.html")
-    context = {
-        "recipes": Recipe.objects.all(),
-    }
-    return HttpResponse(template.render(context, request))
-
-def country_list(request):
     template = loader.get_template("polls/countries.html")
+
+    # Query to get the count of recipes for each country
+    countries_with_counts = Recipe.objects.values('country_of_origin').annotate(recipe_count=Count('id'))
+
+    # Sort countries in descending order based on recipe count
+    sorted_countries = sorted(countries_with_counts, key=lambda x: x['recipe_count'], reverse=True)
+
     context = {
-        "countries": Recipe.objects.values_list('country_of_origin', flat=True).distinct().order_by('country_of_origin'),
+        "countries": [country['country_of_origin'] for country in sorted_countries],
     }
+
     return HttpResponse(template.render(context, request))
 
 def country_recipes(request, country):
     template = loader.get_template("polls/recipes.html")
     context = {
         "recipes": Recipe.objects.filter(country_of_origin=country),
+    }
+    return HttpResponse(template.render(context, request))
+
+def recipe_list(request):
+    template = loader.get_template("polls/recipes.html")
+    context = {
+        "recipes": Recipe.objects.all(),
     }
     return HttpResponse(template.render(context, request))
